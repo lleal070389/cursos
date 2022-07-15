d59bfa094e8c90aa4eeb8865f5f94313f97b9e08
lleal070389 <76750188+lleal070389@users.noreply.github.com>
2022-07-15 07:36:45 -0400

    t1

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
+@app.get('/')
+def index():
+    return render_template("index_t1.html")
+
+
 if __name__ == "__main__":
-    app.config["ENV"] = "development"
     app.run(port=8000, debug=True)
